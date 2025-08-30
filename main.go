9f47a4a75fcddaef59160866e2054f37140d132c
Fumiya Tokumasu <toof@toof.jp>
2025-08-31 00:16:33 +0900

    fix: change env name

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
+	message := os.Getenv("MESSAGE")
+
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("/healthz accessed from %s, MESSAGE=%s", r.RemoteAddr, message)
 		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s", message)
 	})
 
 	log.Println("listening on :8080")
