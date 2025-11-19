dcab9ef96e226f49a67446ae4ecac91e98cc4440
Fumiya Tokumasu <toof@toof.jp>
2025-11-19 22:32:19 +0900

    feat: add secret

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	message := os.Getenv("MESSAGE")
+	secret := os.Getenv("SECRET")
 
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("/healthz accessed from %s, MESSAGE=%s", r.RemoteAddr, message)
+		log.Printf("/healthz accessed from %s, MESSAGE=%s SECRET=%s", r.RemoteAddr, message, secret)
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "%s", message)
+		fmt.Fprintf(w, "%s\n", message, secret)
 	})
 
 	log.Println("listening on :8080")
